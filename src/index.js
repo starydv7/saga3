a2528ee4b2754470d96e22d7700ea9db3a87d6d6
starydv7 <pawanstar72926@gmail.com>
2023-01-13 17:02:25 +0530

    saga3

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Provider } from 'react-redux';
+import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(store);
 root.render(
- 
+ <Provider store={store}>
     <App />
+    </Provider>
  
 );
 
---
+++ src/redux/store.js
@@ -0,0 +1,5 @@
+import { createStore } from "redux";
+const store = createStore(() => {
+    return 100;
+})
+export default store;
