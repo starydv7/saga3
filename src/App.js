f2a8f6a3a63bdc75e5e61465feaa440b53018d26
starydv7 <pawanstar72926@gmail.com>
2023-01-14 23:21:17 +0530

    saga

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { addToCart } from "./redux/action";
+import { useDispatch } from 'react-redux';
 function App() {
+  const dispatch = useDispatch();
+  const products = {
+    name: "I Phone",
+    type: "Mobile",
+    price: 1000,
+    color:"red"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={()=>dispatch(addToCart())}>Add To Cart</button>
     </div>
   );
 }
---
+++ src/redux/action.js
@@ -0,0 +1,7 @@
+export const addToCart = () => {
+    console.log("action called");
+    return {
+        type: "Add_TO_CART",
+        
+    }
+}
