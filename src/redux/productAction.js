e8fa4e9ba9bd8bc7e4cde97a9250161eb9835a76
starydv7 <pawanstar72926@gmail.com>
2023-01-21 21:52:13 +0530

    redux-saga

--- src/redux/productAction.js
+++ src/redux/productAction.js
@@ -1,11 +1,20 @@
 import { PRODUCT_LIST } from "./constant"
 
 export const productList = async () => {
-    let data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    data = await data.json();
-    console.log("action is called", data)
+    // let data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    // data = await data.json();
+    // console.log("action is called", data)
     return {
         type: PRODUCT_LIST,
-        data
+        data:"Apple"
+    }
+}
+export const setproductList = () => {
+    // let data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    // data = await data.json();
+    // console.log("action is called", data)
+    return {
+        type: PRODUCT_LIST,
+        data:"Apple"
     }
 }
