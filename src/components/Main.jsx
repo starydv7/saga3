51fbf2d3813da61a116eee8545067efc6b24ab50
starydv7 <pawanstar72926@gmail.com>
2023-01-23 13:09:33 +0530

    value

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,32 +2,44 @@ import { addToCart, emptyCart, removeToCart } from "../redux/action";
 import { useDispatch } from "react-redux";
 import { productList } from "../redux/productAction";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import styles from "./home.module.css";
 
 function Main() {
   const dispatch = useDispatch();
   let data = useSelector((state) => state.productData);
   console.warn("data in main component", data);
-  const product = {
-    name: "i Phone",
-    category: "mobile",
-    price: 10000,
-    color: "red",
-  };
+
+  useEffect(() => {
+    dispatch(productList());
+  }, []);
   return (
     <div>
-      <button onClick={() => dispatch(addToCart(product))}>Add To Cart</button>
-      <div>
-        <button onClick={() => dispatch(removeToCart(product.name))}>
-          Remove from Cart
-        </button>
-      </div>
       <div>
         <button onClick={() => dispatch(emptyCart())}>Empty Cart</button>
       </div>
-      <div>
-        <button onClick={() => dispatch(productList())}>
-          Call Product List
-        </button>
+      <div className={styles.productcontainer}>
+        {data.map((item) => (
+          <div className={styles.productitem}>
+            <img src={item.thumbnailUrl} alt="" />
+            <div>Name : {item.albumId}</div>
+            <div>
+              {" "}
+              <img src={item.url} alt="" />{" "}
+            </div>
+            <div>Price : {item.title} </div>
+            <div>Category : {item.id} </div>
+
+            <div>
+              <button onClick={() => dispatch(addToCart(item))}>
+                Add to Cart
+              </button>
+              <button onClick={() => dispatch(removeToCart(item.id))}>
+                Remove to Cart
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
