e5201dcc0a40c3aac9b58d0cf52d3fec01933900
starydv7 <pawanstar72926@gmail.com>
2023-01-24 13:04:10 +0530

    value

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import Mian from "./components/Main";
 import './App.css';
 import Headers from './components/Headers';
-import Main from "./components/Main";
-
-
+import Main from './components/Main';
+import {Routes, Route} from 'react-router-dom'
+import Cart from './components/Cart';
 function App() {
   return (
     <div className="App">
       <Headers />
-    <Main/>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
     </div>
   );
 }
